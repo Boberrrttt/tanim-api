3642e0db8763627204abe5e85d51584b46bd7ae4
Robert Salvo <salvo.robertroy@gmail.com>
2026-01-31 10:10:38 +0800

    chore: udpated requirements.txt

--- app/core/database.py
+++ app/core/database.py
@@ -1,21 +1,27 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from ..core.config import DATABASE_URL
-
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"sslmode": "require"}
-)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
+from .config import get_settings  
 
 Base = declarative_base()
 
+def get_engine():
+    settings = get_settings()
+    DATABASE_URL = settings["DATABASE_URL"]
+
+    return create_engine(
+        DATABASE_URL,
+        connect_args={"sslmode": "require"},  
+        pool_pre_ping=True,
+        pool_recycle=300,
+    )
+
 def get_db():
+    engine = get_engine()
+    SessionLocal = sessionmaker(
+        autocommit=False,
+        autoflush=False,
+        bind=engine
+    )
     db = SessionLocal()
     try:
         yield db
