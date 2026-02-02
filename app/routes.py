614957fd765c6ff4b7a844f2e8b6f0252f9a32ca
Robert Salvo <salvo.robertroy@gmail.com>
2026-02-02 21:38:51 +0800

    chore: removed pkl files

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from .modules.farm.routes import router as farm_router
 from .modules.soil_health_test.routes import router as soil_health_test_router
 from .modules.weather.routes import router as weather_router
 from .modules.system.routes import router as system_router
-from .modules.ml.routes import router as ml_router
+# from .modules.ml.routes import router as ml_router
 
 prefix = '/api/v1'
 
@@ -13,4 +13,4 @@ def register_routes(app):
     app.include_router(soil_health_test_router, prefix=prefix)
     app.include_router(weather_router, prefix=prefix)
     app.include_router(system_router, prefix=prefix)
-    app.include_router(ml_router, prefix=prefix)
+    # app.include_router(ml_router, prefix=prefix)
